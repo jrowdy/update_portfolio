545a51c929eb97c31067137c48cb4eec85f8a12c
Jasmin Rowdy <jrowdy@wcpss.net>
2020-11-15 19:49:49 -0500

    Updates

---
+++ precache-manifest.f9a6dc3f644e239b793b9f811d4fd3b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0931e7e0fbf9d6bc243023b92ba5379",
+    "url": "/update_portfolio/index.html"
+  },
+  {
+    "revision": "9f0978825cb3e0405a70",
+    "url": "/update_portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "43ebf4085a5386da334b",
+    "url": "/update_portfolio/static/css/main.46f7e0a1.chunk.css"
+  },
+  {
+    "revision": "9f0978825cb3e0405a70",
+    "url": "/update_portfolio/static/js/2.33ecc009.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/update_portfolio/static/js/2.33ecc009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ebf4085a5386da334b",
+    "url": "/update_portfolio/static/js/main.b71f9efd.chunk.js"
+  },
+  {
+    "revision": "4773623955a1f0f3c254",
+    "url": "/update_portfolio/static/js/runtime-main.ba0649d0.js"
+  },
+  {
+    "revision": "8ece5dbb0a179313abc110ac10b35316",
+    "url": "/update_portfolio/static/media/hi.8ece5dbb.png"
+  }
+]);
